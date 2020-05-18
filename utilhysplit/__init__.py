24802a6a02bad469501bac75315830cbeb088942
amcz <alicec@umd.edu>
2020-05-18 12:26:44 -0400

    add __all__ and __name__ statement to utilhysplit/utilhysplit/__init__.py

--- utilhysplit/__init__.py
+++ utilhysplit/__init__.py
@@ -5,3 +5,7 @@ from . import message_parse,  metfiles, obs_util
 from . import vmixing
 from . import profile
 
+__all__ = ['emitimes', 'forecast_data', 'hcontrol', 'message_parse', 'metfiles',
+            'obs_util', 'vmixing', 'profile']
+
+__name__ = 'utilhysplit'
