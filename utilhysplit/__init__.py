e1ceadd924ae7f34b6956c7c44b4d1c93bed96a5
aring1988 <aring1@umd.edu>
2020-05-29 11:09:12 -0400

    Added python scripts to file so they can be imported and used

--- utilhysplit/__init__.py
+++ utilhysplit/__init__.py
@@ -1,11 +1,8 @@
 #__name__ = 'util'
-#For backward compatability
-from . import emitimes, forecast_data, hcontrol
-from . import message_parse,  metfiles, obs_util
-from . import vmixing
-from . import profile
+# For backward compatability
+from . import cdump2netcdf, concutils, emitimes, emittimes, forecast_data, hcontrol, isoch, lidartrack, message_parse, metdata, metexamples, metfiles, obs_util, par2conc, par2datem, pardat, parutils, profile, read_csv, vmixing
 
-__all__ = ['emitimes', 'forecast_data', 'hcontrol', 'message_parse', 'metfiles',
-            'obs_util', 'vmixing', 'profile']
+__all__ = ['cdump2netcdf', 'concutils', 'emitimes', 'emittimes', 'forecast_data', 'hcontrol', 'isoch', 'lidartrack', 'message_parse',
+           'metdata', 'metexamples', 'metfiles', 'obs_util', 'par2conc', 'par2datem', 'pardat', 'parutils', 'profile', 'read_csv', 'vmixing']
 
 __name__ = 'utilhysplit'
