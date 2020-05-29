17903aa0d37bf05d18a1ba48896add0fe1fa54c0
aring1988 <aring1@umd.edu>
2020-05-29 11:30:21 -0400

    some scripts are not import-able, removed them from the __init__.py file

--- utilhysplit/__init__.py
+++ utilhysplit/__init__.py
@@ -1,8 +1,8 @@
 #__name__ = 'util'
 # For backward compatability
-from . import cdump2netcdf, concutils, emitimes, emittimes, forecast_data, hcontrol, isoch, lidartrack, message_parse, metdata, metexamples, metfiles, obs_util, par2conc, par2datem, pardat, parutils, profile, read_csv, vmixing
+from . import cdump2netcdf, concutils, emitimes, emittimes, forecast_data, hcontrol, message_parse, metdata, metexamples, metfiles, obs_util, parutils, profile, read_csv, vmixing
 
-__all__ = ['cdump2netcdf', 'concutils', 'emitimes', 'emittimes', 'forecast_data', 'hcontrol', 'isoch', 'lidartrack', 'message_parse',
-           'metdata', 'metexamples', 'metfiles', 'obs_util', 'par2conc', 'par2datem', 'pardat', 'parutils', 'profile', 'read_csv', 'vmixing']
+__all__ = ['cdump2netcdf', 'concutils', 'emitimes', 'emittimes', 'forecast_data', 'hcontrol', 'message_parse',
+           'metdata', 'metexamples', 'metfiles', 'obs_util', 'parutils', 'profile', 'read_csv', 'vmixing']
 
 __name__ = 'utilhysplit'
