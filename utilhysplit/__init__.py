e408843bde092a66485d3d6fe7d4095bc6f42538
amcz <alicec@umd.edu>
2020-07-07 11:58:12 -0400

    modified __init__.py to reflect move of emittimes to emitsimple

--- utilhysplit/__init__.py
+++ utilhysplit/__init__.py
@@ -1,5 +1,5 @@
 # For backward compatability
-from . import cdump2netcdf, concutils, emitimes, emittimes, forecast_data, hcontrol, message_parse, metdata, metfiles, obs_util, parutils, profile, vmixing
+from . import cdump2netcdf, concutils, emitimes, emitsimple, forecast_data, hcontrol, message_parse, metdata, metfiles, obs_util, parutils, profile, vmixing
 
 __all__ = ['cdump2netcdf', 'concutils', 'emitimes', 'emittimes', 'forecast_data', 'hcontrol',
            'message_parse', 'metdata',  'metfiles', 'obs_util', 'parutils', 'profile', 'vmixing']
