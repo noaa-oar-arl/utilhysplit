abc8156932b9d509f21e3991cca28e42e9a467e7
amcz <alicec@umd.edu>
2020-10-11 07:33:42 -0400

    changes to poly2netcdf.py

--- utilvolc/poly2netcdf.py
+++ utilvolc/poly2netcdf.py
@@ -12,13 +12,14 @@ def transform_from_latlon(lat,lon):
 
 class VaacPoly():
 
-    def __init__(self, pts, time, tag, height):
+    def __init__(self, pts, time, value, tag, height):
         """
         Helper class for poly2xra
         pts : set of tuples defining a polygon
               (latitude, longitude)
         time : datetime object
                time that polygon is valid for.
+        value : float or int
         tag  : indicates confidence 
         height : in km or FL. 
                 'FL100' or 15
@@ -63,13 +64,103 @@ def process_xra(cxra):
     newxra = newxra.rename(newnames) 
     return newxra 
 
-def poly2xra(vaac_poly_list, cxra, fill=0, tag='poly100', sourceval='human', ensval='E1'):
+
+class Poly2xra:
+
+    def __init__(self):
+        self.sp1 = 'x'
+        self.sp2 = 'y'
+  
+        # create these in create_transform 
+        self.outshape = None
+        self.transform = None
+ 
+    def create_transform(self,cxra):
+        """
+        cxra : DataArray with desired lat, lon shape.
+        """
+        sp1 = 'x'
+        sp2 = 'y'
+        spatial_coords = {sp1:cxra.coords[sp1],sp2:cxra}
+
+        latitude = cxra.latitude  
+        longitude = cxra.longitude
+        poly1= sgeo.Polygon(pts) 
+        outshape = latitude.shape  
+        
+        latitude = cxra.latitude[:,0]
+        longitude = cxra.longitude[0]
+        transform = transform_from_latlon(latitude,longitude)
+
+        self.outshape = outshape
+        self.transform = transform
+        return outshape, transform, latitude, longitude
+
+    def create1(self, 
+                vaac_poly_list,
+                cxra,
+                fill=0,
+                tag='poly100',
+                coordhash={}):
     """
     vaac_poly_list : list of VaacPoly objects
     times should match the times in the cxra
-
     cxra : xarray object with model data
+    
+    fill : value to fill in area around polygon
+     
+    RETURNS:
+    ns2 : DataArray with VAAC polygon.
     """
+        self.create_transform(cxra)
+        iii=0 
+
+        # create the DataArray
+        for vpoly in  vaac_poly_list:
+            
+            poly1 =  vpoly.polygon 
+            raster = features.rasterize([poly1], out_shape=outshape,
+                                     fill=fill, transform=transform,
+                                     dtype=float)
+            newxra = xr.DataArray(raster,coords=spatial_coords, dims=(sp2,sp1))
+
+            coordhash['time'] = vpoly.time
+            ns2 = newxra.assign_coords(coordhash)
+
+            for coordval in coordhash.keys():
+                ns2 = ns2.expand_dims(coordval)
+          
+            # expand in the z dimension 
+            ns2 = self.add_height(ns2,cxra, vpoly.height)        
+
+            if iii==0:
+               polyra = ns2
+            else:
+               polyra = xr.concat([polyra,ns2],'time')
+            iii+=1
+
+        ns2.name = tag
+        return ns2
+
+    def add_height(self, ns2, cxra, polyheight):
+        iii=0
+        concatlist = []
+        for zval in cxra.z.values:
+            ntemp = ns2.assign_coords({'z':zval})
+            # if above top heigh tof polygon set values to 0
+            if zval > polyheight:
+               ntemp = ntemp * 0 
+            concatlist.append(ntemp)
+        # concatenate along z dimension.
+        return xr.concat(concatlist, 'z')  
+
+"""
+    def create1(self,vaac_poly_list, cxra, fill=0, tag='poly100', sourceval='human', ensval='E1'):
+    #vaac_poly_list : list of VaacPoly objects
+    #times should match the times in the cxra
+
+    #cxra : xarray object with model data
+
     # pts: list of tuples with polygon points.
     sp1 = 'x'
     sp2 = 'y'
@@ -86,7 +177,6 @@ def poly2xra(vaac_poly_list, cxra, fill=0, tag='poly100', sourceval='human', ens
     iii=0 
 
     # take the cxra and make a dataset
-
     for vpoly in  vaac_poly_list:
         
         poly1 =  vpoly.polygon 
@@ -118,4 +208,4 @@ def poly2xra(vaac_poly_list, cxra, fill=0, tag='poly100', sourceval='human', ens
     dset = combined_xra.to_dataset(dim='source')
  
  
-    
+"""    
