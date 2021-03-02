3ce318312694c70278d241fa91b043e9a6af1862
aring1988 <aring1@umd.edu>
2021-03-02 13:37:18 -0500

    Added make_ATL_hysp function to make binary xarray with dimensions (source, x, y). Will work to make ATL function incorporate weighting so it can all be done in one step. For now, make_ATL_hysp creates arrays that are used in calculations like brier scores, pattern correlation, CSI, etc., not just the ensemble relative frequency.

--- utilhysplit/evaluation/ensemble_tools.py
+++ utilhysplit/evaluation/ensemble_tools.py
@@ -20,10 +20,11 @@ maxconc   : returns maximum concentration along a dimension(x,y, or z)
 topheight : returns height of level
 ATL       : applied threshold level
 ens_mean  : creates array with ensemble mean values.
+make_ATL_hysp:
 
 PLOTTING
 plotmaxconc : creates plot of maximum concentrations along a dimension (x,y,z)
-getclrslevs : 
+getclrslevs :
 plotATL :
 plot_ens_mean :
 ashcmap :
@@ -38,6 +39,7 @@ maketestra :
 exampleATL
 """
 
+
 def listvals(dra):
     """
     returns 1d list of values in the data-array
@@ -45,6 +47,7 @@ def listvals(dra):
     vals = dra.values
     vshape = np.array(vals.shape)
     return vals.reshape(np.prod(vshape))
+
 
 def ens_cdf(indra, enslist=None, sourcelist=None, timelist=None, threshold=0, plot=True):
     """
@@ -57,73 +60,79 @@ def ens_cdf(indra, enslist=None, sourcelist=None, timelist=None, threshold=0, pl
     dra = choose_and_stack(indra, enslist, sourcelist)
     cdfhash = {}
     for iii, ens in enumerate(dra.ensemble.values):
-        subdra= dra.sel(ensemble=ens)
-        if not isinstance(timelist,list) and not isinstance(timelist,np.ndarray):
-           timelist = [subdra.time.values[0]]
+        subdra = dra.sel(ensemble=ens)
+        if not isinstance(timelist, list) and not isinstance(timelist, np.ndarray):
+            timelist = [subdra.time.values[0]]
         for tm in timelist:
             print(tm, ens)
-            tvals = subdra.sel(time=tm) 
+            tvals = subdra.sel(time=tm)
             # create cdf from values above threshold
             sdata, y = cdf([x for x in listvals(tvals) if x > threshold])
-            cdfhash[(tm,ens[0],ens[1])] = (sdata,y)
-    if plot: plot_cdf(cdfhash)
+            cdfhash[(tm, ens[0], ens[1])] = (sdata, y)
+    if plot:
+        plot_cdf(cdfhash)
     return cdfhash
 
-def plot_cdf(cdfhash,fignum=1):
+
+def plot_cdf(cdfhash, fignum=1):
     fig = plt.figure(1)
-    ax = fig.add_subplot(1,1,1)
-    clrs = ['r','y','g','c','b','k']
+    ax = fig.add_subplot(1, 1, 1)
+    clrs = ['r', 'y', 'g', 'c', 'b', 'k']
     for iii, key in enumerate(cdfhash.keys()):
-        if iii > len(clrs)-1: clrs.extend(clrs)
+        if iii > len(clrs)-1:
+            clrs.extend(clrs)
         ax.step(cdfhash[key][0], cdfhash[key][1], '-'+clrs[iii])
     ax.set_xscale('log')
     return ax
 
+
 def draw_map(fignum, fs=20):
     proj = ccrs.PlateCarree()
-    fig = plt.figure(fignum, figsize=(16,9))
-        
-    ax = fig.add_subplot(1,1,1)
+    fig = plt.figure(fignum, figsize=(16, 9))
+
+    ax = fig.add_subplot(1, 1, 1)
     ax = plt.axes(projection=proj)
-    gl = ax.gridlines(draw_labels=True, linewidth = 0.2, color="gray")
-    gl.labels_right=False
-    gl.labels_top=False
-    #ocean = cfeature.NaturalEarthFeature(category='physical',name='ocean',
+    gl = ax.gridlines(draw_labels=True, linewidth=0.2, color="gray")
+    gl.labels_right = False
+    gl.labels_top = False
+    # ocean = cfeature.NaturalEarthFeature(category='physical',name='ocean',
     #        scale='50m', facecolor = cfeature.COLORE['water'])
 
-    #ax.add_feature(states.edgecolor='gray')
+    # ax.add_feature(states.edgecolor='gray')
     ax.add_feature(cfeature.LAND)
     return ax
+
 
 def exampleATL(revash):
     """
     output of maketestra can be used.
     """
-    #if not revash:
+    # if not revash:
     #    revash = maketestra()
     enslist = revash.ens.values
-    d1 = datetime.datetime(2019,2,25,20)
+    d1 = datetime.datetime(2019, 2, 25, 20)
     level = revash.z.values
     # remove deposition layer if it exists.
     if 0 in level:
-       level = np.delete(level, np.where(level=0))
-    # mult factor should convert unit mass into mg. 
+        level = np.delete(level, np.where(level=0))
+    # mult factor should convert unit mass into mg.
     mult = 1e20
-    fignum=1
+    fignum = 1
     fig = plt.figure(fignum)
     ax = draw_map(fignum)
-    plotATL(ax, revash*mult, time=d1, enslist=enslist, level=level )
+    plotATL(ax, revash*mult, time=d1, enslist=enslist, level=level)
+
 
 def maketestra():
-    flist=[]
-    dname='/pub/ECMWF/JPSS/reventador/ens4/'
-    basefname='cdump.004a.A'
-    elist = [0,1,2,3,4,5,6,7,8,9]
+    flist = []
+    dname = '/pub/ECMWF/JPSS/reventador/ens4/'
+    basefname = 'cdump.004a.A'
+    elist = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     source = 'S1'
     for eee in elist:
         fname = basefname + 'e' + str(eee)
-        flist.append((os.path.join(dname,fname), source, '4ERA5e' + str(eee)))
-    #revash = monetio.models.hysplit.combine_datatset(flist, drange=None)
+        flist.append((os.path.join(dname, fname), source, '4ERA5e' + str(eee)))
+    # revash = monetio.models.hysplit.combine_datatset(flist, drange=None)
     revash = hysplit.combine_dataset(flist)
     return revash
 
@@ -133,7 +142,7 @@ def makenc(dset, ncname):
     time, x, y, z have attributes which cause trouble
     when trying to write a netcdf file.
     """
-    attlist = ['area','proj4_srs']
+    attlist = ['area', 'proj4_srs']
     for att in attlist:
         try:
             del dset.time.attrs[att]
@@ -154,38 +163,42 @@ def makenc(dset, ncname):
     dset.to_netcdf(ncname, format='NETCDF4')
     return -1
 
+
 def readnc(name):
     """
     reads in as dataset.
     convert to dataarray
-    remove variable as a dimension 
+    remove variable as a dimension
     """
     dset = xr.open_dataset(name)
     dra = dset.to_array()
     dra = dra.isel(variable=0)
     return dra
 
-def ashcmap(t=1):
-     """
-     possible colormaps for plotting volcanic ash.
-     """
-     import matplotlib
-     #cmap = matplotlib.cm.cubehelix(np.linspace(0,1,20)) 
-     #cmap = matplotlib.cm.RdGy(np.linspace(0,1,20)) 
-     #cmap = matplotlib.colors.ListedColormap(cmap[10:,:-1])
-     if t==1:
-         cmap = 'RdGy_r'
-     elif t==2:
-         cmap=sns.diverging_palette(200,1,as_cmap=True, center='light',l=10)
-     elif t==3:
-         cmap=sns.diverging_palette(200,1,as_cmap=True, center='dark',l=70)
-     return cmap
 
-#_______________________________________________________________________
+def ashcmap(t=1):
+    """
+    possible colormaps for plotting volcanic ash.
+    """
+    import matplotlib
+    # cmap = matplotlib.cm.cubehelix(np.linspace(0,1,20))
+    # cmap = matplotlib.cm.RdGy(np.linspace(0,1,20))
+    # cmap = matplotlib.colors.ListedColormap(cmap[10:,:-1])
+    if t == 1:
+        cmap = 'RdGy_r'
+    elif t == 2:
+        cmap = sns.diverging_palette(200, 1, as_cmap=True, center='light', l=10)
+    elif t == 3:
+        cmap = sns.diverging_palette(200, 1, as_cmap=True, center='dark', l=70)
+    return cmap
+
+
+# _______________________________________________________________________
 """
 FUNCTIONS
 maxconc : returns maximum concentration along a dimension(x,y, or z)
 """
+
 
 def maxconc(draash, time, enslist, level, dim='y'):
     """
@@ -196,8 +209,8 @@ def maxconc(draash, time, enslist, level, dim='y'):
     level : list of integers (indices of vertical levels to choose).
             should NOT include the deposition layer if dim='y' or dim='x'.
     """
-    source=0
-    #L returns maximum concentration at any level.
+    source = 0
+    # L returns maximum concentration at any level.
     r2 = draash.sel(time=time)
     if 'ens' in draash.dims:
         r2 = r2.isel(ens=enslist)
@@ -213,38 +226,40 @@ def maxconc(draash, time, enslist, level, dim='y'):
             rtot = rtot.isel(ens=0)
     return rtot
 
-def topheight(draash, time,ens, level,thresh=0.01,tp=0):
-     source=0
-     if isinstance(level, int):
-        level=[level]
-     iii=0
-     for lev in level:
-         lev_value = draash.z.values[lev]
-         r2 = draash.sel(time=time)
-         r2 = r2.isel(ens=ens)
-         r2 = r2.isel(source=source, z=lev)
-         # place zeros where it is below threshold
-         r2 = r2.where(r2>=thresh)
-         r2 = r2.fillna(0)
-         # place ones where it is above threshold
-         r2 = r2.where(r2<thresh)
-         r2 = r2.fillna(lev_value)
-         if iii==0: 
+
+def topheight(draash, time, ens, level, thresh=0.01, tp=0):
+    source = 0
+    if isinstance(level, int):
+        level = [level]
+    iii = 0
+    for lev in level:
+        lev_value = draash.z.values[lev]
+        r2 = draash.sel(time=time)
+        r2 = r2.isel(ens=ens)
+        r2 = r2.isel(source=source, z=lev)
+        # place zeros where it is below threshold
+        r2 = r2.where(r2 >= thresh)
+        r2 = r2.fillna(0)
+        # place ones where it is above threshold
+        r2 = r2.where(r2 < thresh)
+        r2 = r2.fillna(lev_value)
+        if iii == 0:
             rtot = r2
             rtot.expand_dims('z')
-         else:
+        else:
             r2.expand_dims('z')
-            rtot = xr.concat([rtot,r2],'z')
-         rht = rtot.max(dim='z')
-     return  rht
+            rtot = xr.concat([rtot, r2], 'z')
+        rht = rtot.max(dim='z')
+    return rht
+
 
 def choose_and_stack(indra, enslist=None, sourcelist=None):
     """
     Inputs:
-    indra : xarray Data Array such as produced by combine_dataset or hysp_massload 
+    indra : xarray Data Array such as produced by combine_dataset or hysp_massload
 
     Returns:
-    dra : xarray Data Array. 
+    dra : xarray Data Array.
           "ens" and "source" dimension have been combined into one dimension. "ensemble"
           If "z" coordinate not present it is added.
     """
@@ -261,17 +276,20 @@ def choose_and_stack(indra, enslist=None, sourcelist=None):
     if sourcelist:
         dra = dra.isel(source=sourcelist)
 
-    dra = dra.stack(ensemble=("ens","source"))
+    dra = dra.stack(ensemble=("ens", "source"))
     return dra
 
 
 def APL(indra, problev=50, enslist=None, sourcelist=None, level=None):
     newra = APLra(indra, enslist, sourcelist, level)
-    vpi = np.where(newra.percent_level.values <=problev)
+    vpi = np.where(newra.percent_level.values <= problev)
     # if pick value lower than one available then just provide lowest level.
-    if not vpi[0]: pindex=0
-    else: pindex = vpi[0][-1]
-    return newra.sel(index=pindex) 
+    if not vpi[0]:
+        pindex = 0
+    else:
+        pindex = vpi[0][-1]
+    return newra.sel(index=pindex)
+
 
 def APLra(indra, enslist=None, sourcelist=None, level=None):
     """
@@ -285,13 +303,13 @@ def APLra(indra, enslist=None, sourcelist=None, level=None):
     # Applied percentile level
     # currently only work for mass loading.
     dra = indra.copy()
-    dra = choose_and_stack(indra,enslist,sourcelist)
+    dra = choose_and_stack(indra, enslist, sourcelist)
     # change this when modifying for concentrations.
     dra2 = dra.isel(z=0)
     # TODO not sure what correct value for transpose_coords should be?
-    dra2 = dra2.transpose('ensemble','y','x',transpose_coords=False) 
+    dra2 = dra2.transpose('ensemble', 'y', 'x', transpose_coords=False)
     coords = dra2.coords
-    coords2 = {'latitude':coords['latitude'],
+    coords2 = {'latitude': coords['latitude'],
                'longitude': coords['longitude'],
                'x': coords['x'],
                'y': coords['y']}
@@ -300,19 +318,20 @@ def APLra(indra, enslist=None, sourcelist=None, level=None):
     # sort along the ensemble axis.
     # sort is an inplace operation. returns an empty array.
     # TODO: check that the ensemble axis is the first one.
-    dvalues.sort(axis=0) 
+    dvalues.sort(axis=0)
     # instead of an 'ensemble' dimension, now have an 'index' dimension.
-    newra = xr.DataArray(dvalues, dims=['index',dims[1],dims[2]], coords=coords2)
-    percents = 100*(newra.index.values+1) / len(newra.index.values)   
-    newra = newra.assign_coords(percent_level=('index',percents))
-    return newra 
+    newra = xr.DataArray(dvalues, dims=['index', dims[1], dims[2]], coords=coords2)
+    percents = 100*(newra.index.values+1) / len(newra.index.values)
+    newra = newra.assign_coords(percent_level=('index', percents))
+    return newra
 
 
 def volcATL(indra):
     newra = indra.MER * indra
     return ATL(indra)
 
-def ATL(indra, enslist=None, sourcelist=None, 
+
+def ATL(indra, enslist=None, sourcelist=None,
         thresh=0.2, level=None, norm=False, weights=1):
     """
      Applied Threshold Level (also ensemble frequency of exceedance).
@@ -321,8 +340,8 @@ def ATL(indra, enslist=None, sourcelist=None,
      enslist : list of values to use fo 'ens' coordinate
      sourcelist : list of values to use for 'source' coordinate
      level : integer or list of integers of vertical levels to use.
-    
-     # weights need to add up to 1. 
+
+     # weights need to add up to 1.
 
      Returns array with number of ensemble members above
      given threshold at each location.
@@ -331,15 +350,15 @@ def ATL(indra, enslist=None, sourcelist=None,
             if False return number of members.
      """
     dra = indra.copy()
-    dra = choose_and_stack(indra,enslist,sourcelist)
+    dra = choose_and_stack(indra, enslist, sourcelist)
     if isinstance(level, int):
         level = [level]
     if not level:
-       mlen = len(dra.z.values) 
-       level = np.arange(0,mlen) 
+        mlen = len(dra.z.values)
+        level = np.arange(0, mlen)
     # not sure if this loop over the levels is needed.
     for iii, lev in enumerate(level):
-        #dra2 = dra.sel(ens=enslist)
+        # dra2 = dra.sel(ens=enslist)
         dra2 = dra.isel(z=lev)
         if "source" in dra2.coords:
             dra2 = dra2.isel(source=source)
@@ -368,21 +387,51 @@ def ATL(indra, enslist=None, sourcelist=None,
         rtot = rtot / nmembers
     return rtot
 
-def ens_mean(draash,time,enslist,level=1):
+
+def ens_mean(draash, time, enslist, level=1):
     """
     draash : xarray
     time : datetime object : date in time coordinate
     enslist: list of integers.
     level : int : index of level in z coordinate.
     """
-    source=0
+    source = 0
     sns.set()
     m2 = draash.sel(time=time)
-    m2 = m2.isel(ens=enslist,source=source)
+    m2 = m2.isel(ens=enslist, source=source)
     m2 = m2.mean(dim=['ens'])
     if level > 0:
         m2 = m2.isel(z=level)
     return m2
 
- 
 
+def make_ATL_hysp(xra, variable='p006', threshold=0.):
+    """
+    Uses threshold value to make binary field of ensemble members.
+    For use in statistics calculations. Based on mass loading, no vertical component
+    Inputs:
+    xra: xarray dataset - netcdf files from hysplit.combine_dataset
+    variable: variable name (string) from xra
+    threshold: ash threshold - default=0. (float/integer)
+    Output:
+    xrabin: binary xarray dataarray (source, x, y)
+    """
+
+    xra2 = xra[variable] * 1000.  # Each vertical layer is 1000m
+    xra2 = xra2.sum(dim='z')  # Summing along z makes the units g/m^2
+    xra2 = xra2[:, 0, 0, :, :]  # Removing ensemble and time dimension, leaving source, x, y
+    # May want to add loops for time and ensemble dimension in the future
+    # MER must used for two ensemble members
+    MER = xra.attrs['Fine Ash MER']
+
+    xra3 = xra2
+    a = 0
+    while a < len(xra2.source):
+        if xra2[a, :, :].source in ('CylinderSource', 'LineSource'):
+            xra3[a, :, :] = xra2[a, :, :] * MER
+        else:
+            xra3[a, :, :] = xra2[a, :, :]
+        a += 1
+
+    xrabin = xr.where(xra3 >= threshold, 1., 0.)
+    return xrabin
