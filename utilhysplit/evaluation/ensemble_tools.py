75091338d444b0740de36380771404392e5d44df
amcz <alicec@umd.edu>
2021-06-02 17:01:02 -0400

    ensemble_tools.py added in some functions accidentally deleted such as ens_cdf.py. applied black formatting. some changes based on pylint output.

--- utilhysplit/evaluation/ensemble_tools.py
+++ utilhysplit/evaluation/ensemble_tools.py
@@ -1,8 +1,5 @@
-import os
 import xarray as xr
 import numpy as np
-import datetime
-import monetio.models.hysplit as hysplit
 from utilhysplit.evaluation.statmain import cdf
 from utilhysplit.evaluation.statmain import pixel_matched_cdf
 
@@ -40,7 +37,7 @@ def APL(indra, problev=50, enslist=None, sourcelist=None):
     try:
         pindex = vpi[0][-1]
     except:
-        pindex=0
+        pindex = 0
     return newra.sel(index=pindex)
 
 
@@ -51,7 +48,7 @@ def APLra(indra, enslist=None, sourcelist=None):
     indra: xr dataarray produced by combine_dataset or by hysp_massload
             it must have 'ens' dimension, 'source' dimension or both.
             time and z dimensions are optional.
- 
+
     enslist : list of values to use for 'ens' coordinate
     sourcelist : list of values to use for 'source' coordinate
 
@@ -61,11 +58,11 @@ def APLra(indra, enslist=None, sourcelist=None):
            and 'index' coordinate.
     """
     # combine 'source' and 'ens' dimensions if applicable.
-    dra2, dim = preprocess(indra,enslist,sourcelist)
+    dra2, dim = preprocess(indra, enslist, sourcelist)
     coords = dra2.coords
     coords2 = dict(coords)
     # remove 'ens' from the coordinate dictionary.
-    coords2.pop('ens')
+    coords2.pop("ens")
     dims = list(dra2.dims)
     # find which dimension is the 'ens' dimension
     dii = dims.index(dim)
@@ -76,18 +73,19 @@ def APLra(indra, enslist=None, sourcelist=None):
     dvalues = dra2.values.copy()
     dvalues.sort(axis=dii)
     # instead of an 'ensemble' dimension, now have an 'index' dimension.
-    dims[dii] = 'index'
+    dims[dii] = "index"
     newra = xr.DataArray(dvalues, dims=dims, coords=coords2)
-    percents = 100*(newra.index.values+1) / len(newra.index.values)
-    newra = newra.assign_coords(percent_level=('index', percents))
+    percents = 100 * (newra.index.values + 1) / len(newra.index.values)
+    newra = newra.assign_coords(percent_level=("index", percents))
     return newra
+
 
 def volcATL(indra):
     newra = indra.MER * indra
     return ATL(indra)
 
 
-def preprocess(indra,enslist=None,sourcelist=None):
+def preprocess(indra, enslist=None, sourcelist=None):
     """
     indra : xarray dataArray
     enslist : list or np.ndarray
@@ -102,47 +100,44 @@ def preprocess(indra,enslist=None,sourcelist=None):
     dra = indra.copy()
 
     # handle whether using 'ens' dimension, 'source' dimension or both.
-    dim = 'ens'
+    dim = "ens"
 
     # if lists are an np.ndarray then testing them with not
     # returns an error.
     if isinstance(sourcelist, np.ndarray):
         pass
     elif "source" in dra.dims and not sourcelist:
-        sourcelist = dra.source.values 
+        sourcelist = dra.source.values
 
     if isinstance(enslist, np.ndarray):
         pass
     elif "ens" in dra.dims and not enslist:
-        enslist = dra.ens.values 
+        enslist = dra.ens.values
 
-
-    if "source" in dra.dims and 'ens' in dra.dims:
+    if "source" in dra.dims and "ens" in dra.dims:
         dra = dra.sel(ens=enslist)
         dra = dra.sel(source=sourcelist)
         dra = dra.stack(ens=("ens", "source"))
 
     elif "source" in dra.dims:
         dra = dra.sel(source=sourcelist)
-        dim = 'source'
+        dim = "source"
     elif "ens" in dra.dims:
         dra = dra.sel(ens=enslist)
     else:
-        print('Warning: could not find source or ens dimension')
+        print("Warning: could not find source or ens dimension")
         dim = None
     return dra, dim
 
 
-
-def ATL(indra, enslist=None, sourcelist=None,
-        thresh=0,  norm=False, weights=None):
+def ATL(indra, enslist=None, sourcelist=None, thresh=0, norm=False, weights=None):
     """
      Applied Threshold Level (also ensemble frequency of exceedance).
 
      indra: xr dataarray produced by combine_dataset or by hysp_massload
             it must have 'ens' dimension, 'source' dimension or both.
             time and z dimensions are optional.
- 
+
      enslist : list of values to use for 'ens' coordinate
      sourcelist : list of values to use for 'source' coordinate
 
@@ -163,30 +158,103 @@ def ATL(indra, enslist=None, sourcelist=None,
 
      """
     # handle whether using 'ens' dimension, 'source' dimension or both.
-    dra, dim = preprocess(indra,enslist,sourcelist)
-    
-    if thresh == 0: 
-        dra2 = xr.where(dra>thresh,1,0)
-    else: 
-        dra2 = xr.where(dra>=thresh,1,0)
+    dra, dim = preprocess(indra, enslist, sourcelist)
 
-    if isinstance(weights,np.ndarray) or isinstance(weights,list):
-       if len(weights) != len(dra2[dim].values):
-          print('WARNING : weights do not coorespond to values')
-          print('weights :', weights)
-          print('values : ', dra2[dim].values)
-       else:
-          wra = xr.DataArray(weights,dims=dim)
-          dra2 = wra * dra2 
+    if thresh == 0:
+        dra2 = xr.where(dra > thresh, 1, 0)
+    else:
+        dra2 = xr.where(dra >= thresh, 1, 0)
+
+    if isinstance(weights, np.ndarray) or isinstance(weights, list):
+        if len(weights) != len(dra2[dim].values):
+            print("WARNING : weights do not coorespond to values")
+            print("weights :", weights)
+            print("values : ", dra2[dim].values)
+        else:
+            wra = xr.DataArray(weights, dims=dim)
+            dra2 = wra * dra2
     dra2 = dra2.sum(dim=[dim])
     if norm:
         nmembers = len(enslist)
-        dra2 = dra2/ nmembers
+        dra2 = dra2 / nmembers
     return dra2
 
 
+def listvals(dra):
+    """
+    returns 1d list of values in the data-array
+    """
+    vals = dra.values
+    vshape = np.array(vals.shape)
+    return vals.reshape(np.prod(vshape))
 
-def make_ATL_hysp(xra, variable='p006', threshold=0., MER=None):
+
+def ens_cdf(
+    indra,
+    enslist=None,
+    sourcelist=None,
+    timelist=None,
+    threshold=0,
+    plot=True,
+    pixel_match=None,
+):
+    """
+    produces plots of cumulative distribution functions.
+    indra : xarray DataArray produced by combine_dataset function or hysp_massload function..
+    timelist : list of times in the time coordinate to produce plots for
+    threshold : float : produce CDF for values > threshold.
+    pixel_match : Number of pixels in observation.
+                  if not None then will use this instead of threshold.
+                  Cut length of modeled data to same length as observed. 
+    Returns:
+    cdfhash : dictionary. key is the time. value is a tuple of the CDF (x,y)
+    """
+    # select sources of interest and stack
+
+    if "source" in indra.dims:
+        sourcekey = True
+    else:
+        sourcekey = False
+    dra = choose_and_stack(indra, enslist, sourcelist)
+    cdfhash = {}
+    if plot:
+        fig = plt.figure(1)
+        ax = fig.add_subplot(1, 1, 1)
+    for iii, ens in enumerate(dra.ens.values):
+        subdra = dra.sel(ens=ens)
+        if not isinstance(timelist, list) and not isinstance(timelist, np.ndarray):
+            timelist = subdra.time.values
+        for tm in timelist:
+            tvals = subdra.sel(time=tm)
+            # create cdf from values above threshold
+            if not pixel_match:
+                sdata, y = cdf([x for x in listvals(tvals) if x > threshold])
+            else:
+                sdata, y = pixel_matched_cdf(listvals(tvals), pixel_match)
+            if sourcekey:
+                key = (tm, ens[0], ens[1])
+            else:
+                key = (tm, ens)
+            cdfhash[key] = (sdata, y)
+    if plot:
+        plot_cdf(ax, cdfhash)
+    return cdfhash
+
+
+def plot_cdf(ax, cdfhash, fignum=1):
+    """
+    Plots output from ens_cdf
+    """
+    clrs = ["r", "y", "g", "c", "b", "k"]
+    for iii, key in enumerate(cdfhash.keys()):
+        if iii > len(clrs) - 1:
+            clrs.extend(clrs)
+        ax.step(cdfhash[key][0], cdfhash[key][1], "-" + clrs[iii])
+    ax.set_xscale("log")
+    return ax
+
+
+def make_ATL_hysp(xra, variable="p006", threshold=0.0, MER=None):
     # amr version. maybe not needed now that ATL has been modified.
     """
     Uses threshold value to make binary field of ensemble members.
@@ -200,27 +268,31 @@ def make_ATL_hysp(xra, variable='p006', threshold=0., MER=None):
     xrabin: binary xarray dataarray (source, x, y)
     """
 
-    xra2 = xra[variable] * 1000.  # Each vertical layer is 1000m - MAY WANT TO ALLOW INPUT
-    xra2 = xra2.sum(dim='z')  # Summing along z makes the units g/m^2
+    xra2 = (
+        xra[variable] * 1000.0
+    )  # Each vertical layer is 1000m - MAY WANT TO ALLOW INPUT
+    xra2 = xra2.sum(dim="z")  # Summing along z makes the units g/m^2
     # May want to add loops for time and ensemble dimension in the future
     # MER must used for two ensemble members
     if MER == None:
-        MER = xra.attrs['Fine Ash MER']  # Allowing for MER input - default is Mastin equation MER
+        MER = xra.attrs[
+            "Fine Ash MER"
+        ]  # Allowing for MER input - default is Mastin equation MER
 
     xra3 = xra2
     a = 0
     while a < len(xra2.source):
-        if xra2[a, :, :].source in ('CylinderSource', 'LineSource'):
+        if xra2[a, :, :].source in ("CylinderSource", "LineSource"):
             xra3[a, :, :] = xra2[a, :, :] * MER
         else:
             xra3[a, :, :] = xra2[a, :, :]
         a += 1
 
-    xrabin = xr.where(xra3 >= threshold, 1., 0.)
+    xrabin = xr.where(xra3 >= threshold, 1.0, 0.0)
     return xrabin
 
 
-def topheight(draash, time, ens, level, thresh=0.01, tp=0):
+def topheight(draash, time, ens, level, thresh=0.01):
     # more work needs to be done on this or there may be
     # a similar function elsewhere.
 
@@ -230,23 +302,20 @@ def topheight(draash, time, ens, level, thresh=0.01, tp=0):
     iii = 0
     for lev in level:
         lev_value = draash.z.values[lev]
-        r2 = draash.sel(time=time)
-        r2 = r2.isel(ens=ens)
-        r2 = r2.isel(source=source, z=lev)
+        rr2 = draash.sel(time=time)
+        rr2 = rr2.isel(ens=ens)
+        rr2 = rr2.isel(source=source, z=lev)
         # place zeros where it is below threshold
-        r2 = r2.where(r2 >= thresh)
-        r2 = r2.fillna(0)
+        rr2 = rr2.where(rr2 >= thresh)
+        rr2 = rr2.fillna(0)
         # place ones where it is above threshold
-        r2 = r2.where(r2 < thresh)
-        r2 = r2.fillna(lev_value)
+        rr2 = rr2.where(rr2 < thresh)
+        rr2 = rr2.fillna(lev_value)
         if iii == 0:
-            rtot = r2
-            rtot.expand_dims('z')
+            rtot = rr2
+            rtot.expand_dims("z")
         else:
-            r2.expand_dims('z')
-            rtot = xr.concat([rtot, r2], 'z')
-        rht = rtot.max(dim='z')
+            rr2.expand_dims("z")
+            rtot = xr.concat([rtot, rr2], "z")
+        rht = rtot.max(dim="z")
     return rht
-
-
-
