fd6da50a7ac8cc5238dc14e5fa916f3b27422b6d
amcz <alicec@umd.edu>
2021-07-13 14:58:25 -0400

    volcat_logic.py captures logic for assimilating data into the HYSPLIT volcat_file.py separates some of the functions for finding volcat files that were in volcat.py into a new file.

---
+++ utilvolc/volcat_logic.py
@@ -0,0 +1,67 @@
+import volcat_files.py
+
+
+
+def workflow():
+    # check for event summary files and read
+    # decide which file list json files to pull
+         # these files change so will need to be re-pulled (or maybe new files are written?).
+    # pull file list json files and read
+    # decide which event files to pull.
+         # check if file already exists.
+         # os.isfile(filename)
+         # check if file meets time resolution requirement (spacing every 10 minutes)
+         # check if file meets other requirements as needed.
+    # pull file(s)
+         # link from json dictionary
+    # make parallax corrected files
+    # make emit-times files
+         # area calculation
+    # some decision logic about when and what kind of HYSPLIT runs we make.
+          # are they automatic
+          # are they triggered by a request from web.
+          # Limited number of runs for active volcano and then link to READY website
+          # where they can choose to modify inputs.
+          # populate with latitude longitude what satellite retrievals are available.etc 
+
+    # run HYSPLIT data insertion
+          # write CONTROL file
+          # write SETUP file
+
+    # regrid volcat to HYSPLIT grid
+          # needed for inverse modeling
+          # needed for evaluation
+          # two possible paths
+            # write regridded file for every volcat retrieval
+            # write hour averaged regridded files.
+
+    # run HYSPLIT inverse modeling
+          # make unit source runs (maybe triggered by web request before data available?)
+          #      (Note number 5 could be an input)
+          #      run duration 5 hours (this means you can only use observations out 5 hours)
+          #      runs that start at hour 0 - 5 hour duration
+          #      runs that start at hour 1 - 4 hour duration
+          #      runs that start at hour 2 - 3 hour duration
+          #      runs that start at hour 3 - 2 hour duration
+          #      runs that start at hour 4 - 1 hour duration
+          #      etc. so they all end at the same time.
+          # pick observation(s) to use in the run.
+          #      at first can only use observations out to hour 5.
+          # For eruptions lasting longer than 5 hours.
+          #      pick up the pardump files from the last runs and continue them for longer.
+          #      runs that start at hour 5 - 5 hour duration
+          #      runs that start at hour 6 - 4 hour duration
+          #        
+          # solve the TCM
+          # make the forecast using the source term.
+
+    # Postprocessing
+          # generate ensemble netcdfs.
+          # ensemble weighting.
+          # graphics
+          
+
+    # Evaluation against observations.
+
+   
+  
