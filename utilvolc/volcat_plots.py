49fb9edc6b19735dff2cb55b861006b76193a2f0
amcz <alicec@umd.edu>
2021-09-22 20:06:52 -0400

    volcat_plot.py added capability to create boxplots for mass loading distributions.

--- utilvolc/volcat_plots.py
+++ utilvolc/volcat_plots.py
@@ -1,10 +1,12 @@
 import numpy as np
+from scipy.stats import describe
 import pandas as pd
 import datetime
 import matplotlib.pyplot as plt
 import seaborn as sns
 import xarray as xr
 from utilvolc import volcat
+from utilhysplit.evaluation import statmain
 
 class VolcatPlots:
 
@@ -34,6 +36,7 @@ class VolcatPlots:
         maxradius = []
         dtlist = []
         time_elapsed=0
+        self.vmass =  []
 
         for iii in np.arange(0,len(das)+1):
             try:
@@ -43,6 +46,7 @@ class VolcatPlots:
             vht  = volcat.get_height(das[iii],clip=True)
             vrad  = volcat.get_radius(das[iii],clip=True)
 
+            self.vmass.append(vmass)  
             maxmass.append(np.max(vmass))
             minmass.append(np.min(vmass))
 
@@ -93,10 +97,102 @@ class VolcatPlots:
         self.minrad = minradius
         self.maxrad = maxradius
 
-    def make_spline(self,s=None):
+    def boxplotdata(self, datelist, vdata):
+        newdata = []
+        mlen=2
+        for ra in vdata:
+            ra = np.array(ra)
+            mlen = np.max([mlen,ra.shape[0]])
+        for ra in vdata:
+            ra = np.array(ra)
+            if ra.shape[0] < mlen:
+               newra = np.pad(ra, (0,mlen-ra.shape[0]),'constant',constant_values=(0,-999))
+            else:
+               newra = ra
+            newdata.append(newra)
+        newdata = np.array(newdata)
+        dj = pd.DataFrame(newdata.T, columns=datelist)
+        dj = dj.replace(-999,np.NaN)
+        self.dj = dj
+        return dj
+
+    def make_boxplot(self, cols=None):
+        sns.set_style('whitegrid')
+        fig = plt.figure(1,figsize=(20,5))
+        if isinstance(cols,(list,np.ndarray)):
+            self.dj.iloc[:,cols].boxplot()
+        else:
+            self.dj.boxplot()
+        ax = plt.gca()
+        ax.set_yscale('log')
+        #ax.set_xlim([d1,d2])
+        fig.autofmt_xdate()
+        plt.show()  
+
+    def volcat_describe_plot(self,threshold=0,nums=None):
+        date = []
+        ens = []
+        mean = []
+        var = []
+        skew = []
+        kurt = []
+        num = []
+        small = []
+        big = []
+        vdata = []
+        
+        clr = ["-m", "-r", "-b", "-c", "-g"]
+        if isinstance(nums,list): vlist = self.vmass[nums[0]:nums[1]]
+        else: vlist = self.vmass
+        for jjj, volc in enumerate(vlist):
+            volc = volc.values.flatten()
+            volc = [x for x in volc if x > threshold]
+            sts = describe(volc)
+            vdata.append(volc)
+            date.append(self.dlist[jjj][0])
+            ens.append('obs')
+            mean.append(sts.mean)
+            var.append(sts.variance)
+            skew.append(sts.skewness)
+            kurt.append(sts.kurtosis)
+            num.append(sts.nobs)
+            small.append(sts.minmax[0])
+            big.append(sts.minmax[1])
+        self.boxplotdata(date,vdata)
+        data = zip(date,ens,mean,var,skew,kurt,num,small,big)
+        colnames = ['date','ens','mean','variance','skewness','kurtosis','N','min','max']
+        dfout = pd.DataFrame(data)
+        dfout.columns = colnames
+        self.dfstats = dfout
+        return dfout
+
+    def volcat_cdf_plot(self, threshold=0, nums=None):
+        #step = 5
+        clr = ["-m", "-r", "-b", "-c", "-g"]
+        if isinstance(nums,list): vlist = self.vmass[nums[0]:nums[1]]
+        else: vlist = self.vmass
+        for jjj, volc in enumerate(vlist):
+            # print(self.cdump.time.values[iii])
+            #volcat = self.volcat_avg_hash[iii]
+            volc = volc.values.flatten()
+            volc = [x for x in volc if x > threshold]
+            try:
+                sdata, y = statmain.cdf(volc)
+            except:
+                print("cannot calculate cdf for {}".format(jjj))
+                continue
+            ax = plt.gca()
+            if jjj % 5 == 0:
+                lw = 3
+                # print('here')
+            else:
+                lw = 1
+            ax.step(sdata, y, clr[jjj % len(clr)], LineWidth=lw)
+        return ax
+
+    def make_spline(self,s=20):
         import scipy.interpolate 
-        if not s:
-           s = 2.0 / len(self.dtlist)
+        s = s / len(self.dtlist)
         #self.spline = scipy.interpolate.CubicSpline(self.dtlist,self.tmasslist)
         self.spline = scipy.interpolate.UnivariateSpline(self.dtlist,self.tmasslist,s=s)
 
@@ -119,8 +215,33 @@ class VolcatPlots:
 
         return fig
 
-    def plot_multiA(self,fignum=1):
-        self.make_spline()
+    def plot_dist_stats(self,fignum=2):
+        sns.set_style('whitegrid')
+        fig = plt.figure(fignum,figsize=[10,10])
+
+        ax1 = fig.add_subplot(2,2,1)
+        self.sub_plot_mean(ax1)
+
+
+        ax2 = fig.add_subplot(2,2,2)
+        self.sub_plot_variance(ax2)
+
+        ax3 = fig.add_subplot(2,2,3)
+        self.sub_plot_skew(ax3)
+
+        ax4 = fig.add_subplot(2,2,4)
+        self.sub_plot_kurt(ax4)
+
+        ax5 = ax4.twinx()
+        self.sub_plot_num(ax5)
+        ax5.grid(False)
+
+        fig.autofmt_xdate()
+        plt.tight_layout()
+        return fig
+
+    def plot_multiA(self,fignum=1,smooth=20):
+        self.make_spline(s=smooth)
 
         sns.set_style('whitegrid')
         fig = plt.figure(fignum,figsize=[10,10])
@@ -151,16 +272,77 @@ class VolcatPlots:
         ax.plot(xval,yval3,self.sub_clrs[1],label='Maximum')
         ax.set_ylabel('Effective radius')
         ax.set_xlabel('Time')
-        
-    def sub_plot_mass(self,ax):
+       
+    def fit_exp_decay(self,xval,yval):
+        ylogval = np.log(yval)
+        k, alog = np.polyfit(xval,ylogval,1)
+        return k, alog  
+
+    def sub_plot_mean(self,ax):
+        xval = self.dfstats['date']
+        yval = self.dfstats['mean']
+        yval2 = self.dfstats['min']
+        yval3 = self.dfstats['max']
+        ax.plot(xval,yval,self.main_clr,label='mean')
+        ax.plot(xval,yval2,'--k',label='min')
+        ax.plot(xval,yval3,'--k',label='max')
+        ax.set_ylabel('mass loading (g m$^{-2}$)')
+        ax.set_xlabel('Time')
+        handles, labels = ax.get_legend_handles_labels()
+        ax.legend(handles,labels)
+        ax.set_yscale('log')
+
+    def sub_plot_variance(self,ax):
+        xval = self.dfstats['date']
+        yval = self.dfstats['variance']
+        ax.plot(xval,yval,self.main_clr)
+        ax.set_ylabel('variance')
+        ax.set_xlabel('Time')
+
+    def sub_plot_kurt(self,ax):
+        xval = self.dfstats['date']
+        yval = self.dfstats['kurtosis']
+        ax.plot(xval,yval,self.main_clr)
+        ax.set_ylabel('Kurtosis')
+        ax.set_xlabel('Time')
+       
+       
+
+    def sub_plot_skew(self,ax):
+        xval = self.dfstats['date']
+        yval = self.dfstats['skewness']
+        ax.plot(xval,yval,self.main_clr)
+        ax.set_ylabel('Skewness')
+        ax.set_xlabel('Time')
+       
+       
+    def sub_plot_num(self,ax):
+        xval = self.dfstats['date']
+        yval = self.dfstats['N']
+        ax.plot(xval,yval,'-k',Linewidth=5,alpha=0.5)
+        ax.set_ylabel('Number of Observations')
+        ax.set_xlabel('Time')
+       
+
+ 
+    def sub_plot_mass(self,ax,yscale='ln'):
+        import matplotlib.ticker as mtick
+        def ticks(y,pos):
+            pstr = '{:.0f}'.format(np.log(y))
+            return r'$e^{}$'.format(pstr)
+
         yval = self.tmasslist
         xval = self.dlist
 
         ys = self.spline(self.dtlist)
-        ax.plot(xval,ys,self.spline_clr, LineWidth=5,alpha=0.5)
-
+        #ax.plot(xval,ys,self.spline_clr, LineWidth=3,alpha=0.6)
+        eee = 80
+        #ax.plot(xval[20:eee],yval[20:eee],self.main_clr)
         ax.plot(xval,yval,self.main_clr)
-
+        #ax.plot(xval,np.log(yval),self.main_clr)
+        if yscale == 'ln': 
+           ax.semilogy(basey=np.e) 
+           ax.yaxis.set_major_formatter(mtick.FuncFormatter(ticks)) 
 
         ax.set_ylabel('Total mass (Tg)')
         ax.set_xlabel('Time')
@@ -193,15 +375,20 @@ class VolcatPlots:
         ax.set_ylabel('Total Area (km$^2$)')
         ax.set_xlabel('Time')
 
-    def sub_plot_mer(self,ax):
+    def sub_plot_mer(self,ax, yscale='linear', spline=False):
         yval = self.mer
         xval = self.dlist
-        #ax.plot(xval,yval,self.main_clr)
-        mer = self.spline.derivative()
-        ys = mer(self.dtlist)
-        ax.plot(xval,ys*1e9,self.spline_clr, LineWidth=5,alpha=0.5)
+        if not spline:
+            ax.plot(xval,yval,self.main_clr)
+        else:
+            mer = self.spline.derivative()
+            ys = mer(self.dtlist)
+            ax.plot(xval,ys*1e9,'r.', LineWidth=2,alpha=0.8)
+            #ax.plot(xval,-1*ys*1e9,'k.', LineWidth=2,alpha=0.8)
         ax.set_ylabel('kg s$^{-1}$')
         ax.set_xlabel('Time')
+        #ax.semilogy(basey=np.e) 
+        if yscale=='log': ax.set_yscale('log')
 
     def sub_plot_maxht(self,ax):
         yval = self.htlist
