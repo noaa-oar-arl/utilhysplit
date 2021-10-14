2e077169ec2c19559f4378c836f81b9a1d172812
aring1988 <aring1@umd.edu>
2021-10-14 09:39:05 -0400

    tropomi.py: In progress. Function to convert regridded tropomi text files from HyunCheol into Xarray Dataset for use with volcat tools already developed.

---
+++ utilvolc/tropomi.py
@@ -0,0 +1,52 @@
+# tropomi.py
+# A reader/converter for tropomi data
+
+import xarray as xr
+import numpy as np
+import pandas as pd
+from glob import glob
+
+
+def make_nc(filelist, latfile, lonfile, varname='SO2', netdir=None, write=False):
+    """ To convert Hyun Cheol's regridded text files to a netcdf.
+    Inputs:
+    filelist: list of full data filenames (including directory)
+    latfile: full filename containing latitudes (string)
+    lonfile: full filename containing longitudes (string)
+    varname: name of data variable (string)
+    netdir: default=None - netcdf file directory (string) if writing netcdf file
+    write: boolean
+    Outputs:
+    xarray dataarray
+    If write=True, netcdf file is made
+    """
+    # Read in lat/lon files
+    lat = pd.read_csv(latfile, delimiter='\s+', header=None)
+    lon = pd.read_csv(lonfile, delimiter='\s+', header=None)
+    # Convert to numpy
+    latnp = lat.to_numpy()
+    lonnp = lon.to_numpy()
+    # Convert to xarray
+    latxr = xr.DataArray(data=latnp, dims=('x', 'y'), name='latitude')
+    lonxr = xr.DataArray(data=lonnp, dims=('x', 'y'), name='longitude')
+    # Creating original array
+    so2xr = xr.merge([latxr, lonxr])
+    so2xr = so2xr.expand_dims('time')
+    for f in filelist:
+        data = pd.read_csv(f, delimiter='\s+', header=None)
+        datanp = data.to_numpy()
+        dataxr = xr.DataArray(data=datanp, dims=('x', 'y', 'time'), name=varname)
+
+        so2xr = xr.merge([so2xr, dataxr])
+        so2xr = so2xr.set_coords('latitude')
+        so2xr = so2xr.set_coords('longitude')
+        so2xr = so2xr.set_coords('time')
+
+        if write:
+            start = f.rfind('/')+1
+            end = f.find('.txt')
+            netfile = netdir+f[start:end]+'.nc'
+            so2xr.to_netcdf(netfile)
+            print(netfile+' created!')
+
+    return so2xr
