0880c1eacc73325e51d398ae9940a7bdbcfacfe1
amcz <alicec@umd.edu>
2021-12-22 16:42:33 -0500

    ashdatainsertion.py bugfixes

--- utilvolc/ashapp/ashdatainsertion.py
+++ utilvolc/ashapp/ashdatainsertion.py
@@ -2,6 +2,7 @@
 
 #from abc import ABC, abstractmethod
 import datetime
+import numpy as np
 import logging
 import os
 import time
@@ -45,6 +46,8 @@ class DataInsertionRun(AshRun):
 
 
     def add_inputs(self, inp):
+        inp['emissionHours']=0
+        inp['samplingIntervalHours']=1
         inp['WORK_DIR'] = os.path.join(inp['WORK_DIR'],
                                             inp['VolcanoName'],
                                             'emitimes/')
@@ -63,16 +66,18 @@ class DataInsertionRun(AshRun):
         get start date and number of locations from emit file.
         """
         self.file_not_found_error(emitfile, message=True)
-        etf = emitimes.EmitTimes(filename=emitfile)
+        etf = EmiTimes(filename=emitfile)
+        self.inp['emitfile'] = emitfile
         etf.read_file(num_species=1)
         # look at first emission cycle 
         ecycle = etf.cycle_list[0]
+        #print('ecycle', ecycle)
         # number of locations that need to be in CONTROL file.
-        nlocs = ecycle.nlocs 
+        nlocs = ecycle.nrecs
+        logger.info('{} number of locations'.format(nlocs))
         # starting date of this cycle      
         sdate = ecycle.sdate
         self.inp['start_date'] = sdate
-
         # duration of this cycle
         #cduration = ecycle.duration
 
@@ -86,22 +91,40 @@ class DataInsertionRun(AshRun):
         self.inp['area'] = 0
         self.inp['bottom'] = 0
         self.inp['top'] = 0
+        self.inp['nlocs'] = nlocs
         return nlocs
 
     def setup_setup(self,stage):
-        super().setup_setup(stage)
-        setup.add("efile",self.inp['emitfile'])
+        setup = super().setup_setup(stage)
+        eloc = self.inp['emitfile'].split('/')
+        eloc = eloc[-1]
+        setup.add("efile",eloc)
+        return setup
+
+    def setup_basic_control(self,stage=0,rtype='dispersion'):
+        emitfile = find_emit_file(self.inp['WORK_DIR'])
+        nlocs = self.read_emittimes(emitfile)
+        control = super().setup_basic_control(stage=stage,rtype=rtype)
+        return control
 
     def additional_control_setup(self, control, stage=0):
-        emitfile = find_emit_file(self.inp['WORK_DIR'])
+        #emitfile = find_emit_file(self.inp['WORK_DIR'])
         # get number of locations from emit-times file
         # and set some values in inp needed for the control setup.
-        nlocs = self.read_emittimes(emitfile)
-        super().additional_control_setup()
+        #nlocs = self.read_emittimes(emitfile)
+        nlocs = self.inp['nlocs']
+        super().additional_control_setup(control,stage=stage)
         # add as many dummy locations as in emit-times file
         control.remove_locations()
-        for loc in nlocs:
+        lat = self.inp['latitude']
+        lon = self.inp['longitude']
+        vent = self.inp['bottom']
+        area = self.inp['area']
+        rate = self.inp['rate']
+        for loc in np.arange(0,nlocs):
             control.add_location((lat,lon),vent,rate=rate,area=area) 
+        #control.concgrids[0].outfile = self.filelocator.get_cdump_filename(stage)
+        #control.concgrids[0].outdir = self.inp['WORK_DIR']
 
     def file_not_found_error(self, fln, message=False):
         if not os.path.exists(fln):
