82f8c5f76cac4a7bda50685aedcc08666599e0df
amcz <alicec@umd.edu>
2023-01-04 13:06:53 -0500

    change to __init__.py

--- utilvolc/__init__.py
+++ utilvolc/__init__.py
@@ -1,5 +1,5 @@
-from . import volcalert, volcat, volcMER, volcat_so2, raikoke_data, read_csv, hysp_func, ashapp
+from . import volcalert, volcat, volcMER, volcat_so2, read_csv, hysp_func
 
-__all__ = ['volcalert', 'volcat', 'volcMER', 'volcat_so2', 'raikoke_data', 'read_csv', 'hysp_func','ashapp']
+__all__ = ['volcalert', 'volcat', 'volcMER', 'volcat_so2', 'read_csv', 'hysp_func']
 
 __name__ = 'utilvolc'
