5562a14252213da2fbbc5b6432a7de69f45158d0
amcz <alicec@umd.edu>
2023-02-26 15:56:07 -0500

    added tests and test_volcat.py to utilvolc directory for testing of modules in utilvolc. Updated test_hysplit.py  and test_metdata.py in tests directory.

--- tests/test_hysplit.py
+++ tests/test_hysplit.py
@@ -1,13 +1,15 @@
 import datetime
-import numpy as np
-from monetio.models import hysplit
-from utilhysplit import hysplit_gridutil
-import pytest
 import os
 
-def makera(grid):
-    return -1    
+import numpy as np
 
+# from utilhysplit import hysplit_gridutil
+import pytest
+from monetio.models import hysplit
+
+
+def makera(grid):
+    return -1
 
 
 def test_003():
@@ -19,37 +21,37 @@ def test_003():
     # center = 1
     # spacing = 0.5
     # span = 360
-    # -------------------- 
+    # --------------------
     # Latitude
     # center = 1
     # spacing = 0.5
     # span = 180.0
     # ------------
-    attrs = {'llcrnr latitude'   : -90.0,
-             'llcrnr longitude'  : -180.0,
-             'Latitude Spacing'  : 0.5,
-             'Longitude Spacing' : 0.5,
-             'Number Lat Points' : 361,
-             'Number Lon Points' : 721}
-    xxx = [1,5,93]
-    xanswers = [165.0,167.0,-149.0]
-    yyy = [1,17,93]
-    yanswers = [0.0,8.0,46.0]
+    attrs = {
+        "llcrnr latitude": -90.0,
+        "llcrnr longitude": -180.0,
+        "Latitude Spacing": 0.5,
+        "Longitude Spacing": 0.5,
+        "Number Lat Points": 361,
+        "Number Lon Points": 721,
+    }
+    xxx = [1, 5, 93]
+    xanswers = [165.0, 167.0, -149.0]
+    yyy = [1, 17, 93]
+    yanswers = [0.0, 8.0, 46.0]
     grid = hysplit.get_latlongrid(attrs, xxx, yyy)
     latlist, lonlist = hysplit.getlatlon(attrs)
     gridanswer = np.meshgrid(xanswers, yanswers)
-     
 
-    assert len(latlist)== 361
-    assert len(lonlist)== 721
+    assert len(latlist) == 361
+    assert len(lonlist) == 721
     # check that lat lon begin and end in correct place.
     assert latlist[0] == -90.0
     assert latlist[-1] == 90.0
-    assert lonlist[0]  ==  -180.0
+    assert lonlist[0] == -180.0
     # should go -180 to 180 or -180 to -180?
-    assert lonlist[-1] ==  180.0
-    assert lonlist[-2] ==  179.5
-
+    assert lonlist[-1] == 180.0
+    assert lonlist[-2] == 179.5
 
 
 def test_002():
@@ -78,37 +80,38 @@ def test_002():
     # y=1  latitude=0.0
     # y=17 latitude=8
     # y=93 latitude=46.0
-   
 
-    attrs = {'llcrnr latitude'   : 0.0,
-             'llcrnr longitude'  : 165,
-             'Latitude Spacing'  : 0.5,
-             'Longitude Spacing' : 0.5,
-             'Number Lat Points' : 181.0,
-             'Number Lon Points' : 181.0}
-    xxx = [1,5,93]
-    xanswers = [165.0,167.0,-149.0]
-    yyy = [1,17,93]
-    yanswers = [0.0,8.0,46.0]
+    attrs = {
+        "llcrnr latitude": 0.0,
+        "llcrnr longitude": 165,
+        "Latitude Spacing": 0.5,
+        "Longitude Spacing": 0.5,
+        "Number Lat Points": 181.0,
+        "Number Lon Points": 181.0,
+    }
+    xxx = [1, 5, 93]
+    xanswers = [165.0, 167.0, -149.0]
+    yyy = [1, 17, 93]
+    yanswers = [0.0, 8.0, 46.0]
     grid = hysplit.get_latlongrid(attrs, xxx, yyy)
     latlist, lonlist = hysplit.getlatlon(attrs)
 
     gridanswer = np.meshgrid(xanswers, yanswers)
     assert np.array_equal(grid, gridanswer)
 
-    assert len(latlist)== 181
-    assert len(lonlist)== 181
+    assert len(latlist) == 181
+    assert len(lonlist) == 181
     # check that lat lon begin and end in correct place.
     assert latlist[0] == 0.0
     assert latlist[-1] == 90.0
     assert lonlist[0] == 165
     assert lonlist[-1] == -105.0
 
-    for x in zip(xxx,xanswers):
-        assert lonlist[x[0]-1] == x[1]
+    for x in zip(xxx, xanswers):
+        assert lonlist[x[0] - 1] == x[1]
 
-    for y in zip(yyy,yanswers):
-        assert latlist[y[0]-1] == y[1]
+    for y in zip(yyy, yanswers):
+        assert latlist[y[0] - 1] == y[1]
 
 
 def test_001():
@@ -136,35 +139,38 @@ def test_001():
     # nlat = 101
     # y=1  latitude=40.0
     # y=50 latitude=44.9
-    # y=73 latitude=47.2 
+    # y=73 latitude=47.2
 
-    attrs = {'llcrnr latitude'   : 40.0,
-             'llcrnr longitude'  : -155,
-             'Latitude Spacing'  : 0.1,
-             'Longitude Spacing' : 0.1,
-             'Number Lat Points' : 101.0,
-             'Number Lon Points' : 101.0}
+    attrs = {
+        "llcrnr latitude": 40.0,
+        "llcrnr longitude": -155,
+        "Latitude Spacing": 0.1,
+        "Longitude Spacing": 0.1,
+        "Number Lat Points": 101.0,
+        "Number Lon Points": 101.0,
+    }
 
-    xxx = [1,33,69]
-    yyy = [1,50,73]
-    xanswers = [-155,-151.8,-148.2]
-    yanswers = [40.0,44.9,47.2]
+    xxx = [1, 33, 69]
+    yyy = [1, 50, 73]
+    xanswers = [-155, -151.8, -148.2]
+    yanswers = [40.0, 44.9, 47.2]
     grid = hysplit.get_latlongrid(attrs, xxx, yyy)
     gridanswer = np.meshgrid(xanswers, yanswers)
     assert np.array_equal(grid, gridanswer)
- 
+
     latlist, lonlist = hysplit.getlatlon(attrs)
-    assert len(latlist)== 101
-    assert len(lonlist)== 101
+    assert len(latlist) == 101
+    assert len(lonlist) == 101
     assert latlist[0] == 40.0
     assert latlist[-1] == 50.0
     assert lonlist[0] == -155.0
-    assert lonlist[-1] == -145.0 
+    assert lonlist[-1] == -145.0
 
-    for x in zip(xxx,xanswers):
-        assert lonlist[x[0]-1] == x[1]
+    for x in zip(xxx, xanswers):
+        assert lonlist[x[0] - 1] == x[1]
 
-    for y in zip(yyy,yanswers):
-        assert latlist[y[0]-1] == y[1]
+    for y in zip(yyy, yanswers):
+        assert latlist[y[0] - 1] == y[1]
 
-test_001()
+
+# test_001()
--- tests/test_metdata.py
+++ tests/test_metdata.py
@@ -1,18 +1,72 @@
 import datetime
-from utilvolc.ashapp import metfiles
-import pytest
 import os
+
+import pytest
+from utilhysplit import metfiles
+
+
+def test_002():
+    # tests for gfs0p25 archive data
+    adir = "/pub/archives/"
+    metid = "gfs0p25"
+    finder = metfiles.MetFileFinder(metid)
+    finder.set_archives_directory(adir)
+    year = 2020
+    day = 31
+    month = 12
+    hour = 6
+    dstart = datetime.datetime(year, month, day, hour)
+
+    def subfunc(dstart, hours):
+        mdir = "/pub/archives/gfs0p25/"
+        mf = dstart.strftime("%Y%m%d_gfs0p25")
+        answer = [(mdir, mf)]
+        dstart += datetime.timedelta(hours=hours)
+        mf = dstart.strftime("%Y%m%d_gfs0p25")
+        answer.append((mdir, mf))
+        dstart += datetime.timedelta(hours=hours)
+        mf = dstart.strftime("%Y%m%d_gfs0p25")
+        answer.append((mdir, mf))
+        return answer
+
+    # test forward
+    duration = 48
+    mff = finder.find_archive(dstart, duration)
+    answer = subfunc(dstart, 24)
+    # print(mff)
+    # print(answer)
+    assert answer == mff
+
+    mff = finder.find_archive(dstart, duration)
+    answer = subfunc(dstart, 24)
+    assert answer == mff
+    # test backward
+    duration = -48
+    mff = finder.find_archive(dstart, duration)
+    answer = subfunc(dstart, -24)
+    answer.reverse()
+    assert answer == mff
+
+
+# def test_001():
+#    # tests for forecast data
+#    fdir = '/pub/forecast/'
+
+# tests for gfs
+#    metid='gfs'
+#    suffix='gfsf'
+
 
 def test_001():
     # tests for forecast data
-    fdir = '/pub/forecast/'
-    
+    fdir = "/pub/forecast/"
+
     # tests for gfs
-    metid='gfs'
-    suffix='gfsf'
+    metid = "gfs"
+    suffix = "gfsf"
     finder = metfiles.MetFileFinder(metid)
-    finder.set_forecast_directory(fdir)    
-    finder.set_archives_directory('/pub/archive/') 
+    finder.set_forecast_directory(fdir)
+    finder.set_archives_directory("/pub/archive/")
 
     # test on files the day before today.
     dtemp = datetime.datetime.now() - datetime.timedelta(hours=24)
@@ -25,52 +79,47 @@ def test_001():
     duration = 1
 
     # look at '00' cycle
-    rfiles = finder.find_forecast_cycle(dstart, duration, '00')
+    rfiles = finder.find_forecast_cycle(dstart, duration, "00")
     rstr = dstart.strftime("%Y%m%d")
-    answer = ['{}{}/hysplit.t00z.{}'.format(fdir,rstr,suffix)]
+    answer = ["{}{}/hysplit.t00z.{}".format(fdir, rstr, suffix)]
     assert rfiles == answer
 
     # look at other cycle
     dtemp2 = dstart - datetime.timedelta(hours=24)
     rstr = dtemp2.strftime("%Y%m%d")
-    for cycle in ['06','12','18']:
+    for cycle in ["06", "12", "18"]:
         rfiles = finder.find_forecast_cycle(dstart, duration, cycle)
-        answer = ['{}{}/hysplit.t{}z.{}'.format(fdir,rstr,cycle,suffix)]
+        answer = ["{}{}/hysplit.t{}z.{}".format(fdir, rstr, cycle, suffix)]
         assert rfiles == answer
 
     # start at 01 the day before today and run backwards 6 hours.
     duration = -6
-    for cycle in ['00','06','12','18']:
+    for cycle in ["00", "06", "12", "18"]:
         rfiles = finder.find_forecast_cycle(dstart, duration, cycle)
         rstr = dtemp2.strftime("%Y%m%d")
-        answer = ['{}{}/hysplit.t{}z.{}'.format(fdir,rstr,cycle,suffix)]
+        answer = ["{}{}/hysplit.t{}z.{}".format(fdir, rstr, cycle, suffix)]
         rstr = dstart.strftime("%Y%m%d")
-        answer.append('{}{}/hysplit.t{}z.{}'.format(fdir,rstr,cycle,suffix))
+        answer.append("{}{}/hysplit.t{}z.{}".format(fdir, rstr, cycle, suffix))
         assert rfiles == answer
 
     # start at 6z the day before today and run forward 48 hours
-    duration=24 
+    duration = 24
     dstart = datetime.datetime(year, month, day, 6)
-    for cycle in ['00','06','12','18']:
+    for cycle in ["00", "06", "12", "18"]:
         answer = []
         rfiles = finder.find_forecast_cycle(dstart, duration, cycle)
-        if cycle in ['12','18']:
+        if cycle in ["12", "18"]:
             dtemp2 = dstart - datetime.timedelta(hours=24)
             rstr = dtemp2.strftime("%Y%m%d")
-            answer.append('{}{}/hysplit.t{}z.{}'.format(fdir,rstr,cycle,suffix))
+            answer.append("{}{}/hysplit.t{}z.{}".format(fdir, rstr, cycle, suffix))
         rstr = dstart.strftime("%Y%m%d")
-        answer.append('{}{}/hysplit.t{}z.{}'.format(fdir,rstr,cycle,suffix))
+        answer.append("{}{}/hysplit.t{}z.{}".format(fdir, rstr, cycle, suffix))
         dtemp2 = dstart + datetime.timedelta(hours=24)
         rstr = dtemp2.strftime("%Y%m%d")
-       
-        if os.path.isfile('{}{}/hysplit.t{}z.{}'.format(fdir,rstr,cycle,suffix)):
-            answer.append('{}{}/hysplit.t{}z.{}'.format(fdir,rstr,cycle,suffix))
-        print(rfiles)
-        print(answer)
-        #assert rfiles == rstr
 
-test_001() 
+        if os.path.isfile("{}{}/hysplit.t{}z.{}".format(fdir, rstr, cycle, suffix)):
+            answer.append("{}{}/hysplit.t{}z.{}".format(fdir, rstr, cycle, suffix))
+        # assert rfiles == rstr
 
 
-   
- 
+test_001()
