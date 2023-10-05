3cf631b69693c1391800009fe6bc43365cf7a00c
Alice Crawford <alicec@asmd.arl.noaa.gov>
2023-10-05 16:16:37 -0400

    added test_versions.py to help keep track of package versions. Some issues with reading netcdf files with xarray were encountered after trying to install sklearn. The xarray seemed to have been downgraded and had trouble with the time field in the netcdf files.

---
+++ tests/test_versions.py
@@ -0,0 +1,52 @@
+
+
+import xarray
+print('xarray', xarray.__version__)
+print('tested version is 2023.6.0 \n\n')
+
+import netCDF4
+print('netcdf4',netCDF4.__version__)
+print('tested version is 1.6.2 \n\n')
+
+import seaborn
+print('seaborn', seaborn.__version__)
+print('tested version is 0.12.2 \n\n')
+
+import lxml
+print('lxml', lxml.__version__)
+print('tested version is 4.9.3 \n\n')
+
+import scipy
+print('scipy', scipy.__version__)
+print('tested version is 1.11.1 \n\n')
+
+import seaborn
+print('seaborn', seaborn.__version__)
+print('tested version is 0.12.2 \n\n')
+
+import cartopy
+print('cartopy', cartopy.__version__)
+print('tested version is 0.21.1 \n\n')
+
+import shapely
+print('shapely', shapely.__version__)
+print('tested version is 2.0.1 \n\n')
+
+import pandas
+print('pandas', pandas.__version__)
+print('tested version is 2.0.3\n\n')
+
+import numpy
+print('numpy', numpy.__version__)
+print('tested version is 1.26.0\n\n')
+
+import matplotlib
+print('matplotlib', matplotlib.__version__)
+print('tested version is 3.7.2\n\n')
+
+try:
+     import sklearn
+     print('sklearn', sklearn.__version__)
+except:
+     print('sklearn not installed') 
+
