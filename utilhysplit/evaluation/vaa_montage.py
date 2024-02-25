0b9269596f5cedd8e75d6469caef901204ad5954
Alice Crawford <alicec@asmd.arl.noaa.gov>
2024-02-25 15:13:30 -0500

    add vaa_montage.py

---
+++ utilhysplit/evaluation/vaa_montage.py
@@ -0,0 +1,351 @@
+import datetime
+import logging
+import matplotlib.pyplot as plt
+from matplotlib.colors import BoundaryNorm
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import xarray as xr
+
+from utilhysplit.evaluation.ensemble_tools import topheight
+from utilhysplit.plotutils.map_util import get_transform, format_plot, set_ticks
+import ashapp.utils as utils
+import utilhysplit.evaluation.web_ensemble_plots as wep
+from monetio.models import hysplit
+
+logger = logging.getLogger(__name__)
+utils.setup_logger(level=logging.WARNING)
+
+class VAAMontage:
+
+    def __init__(self, cdumpra, 
+                 columns=3,cmap='viridis',
+                 ):
+
+        self._source = None
+        self._ens = None
+        self.cdump = cdumpra
+        self._unit = 'mg m$^{-3}$'
+        self.columns = columns
+        self.cmap = cmap
+        self.get_sloc_fromatts()
+        self.central_longitude = 0    
+        sns.set_style('white')
+
+    @property
+    def cdump(self):
+        return self._cdump
+
+    @cdump.setter
+    def cdump(self,cset):
+        if isinstance(cset,xr.core.dataset.Dataset):
+           varnames = list(cset.data_vars.keys())
+           if len(varnames)>1: 
+              logger.warning('Multiple variables in dataset')
+              logger.warning(varnames)
+           self._cdump = cset[varnames[0]]
+        elif isinstance(cset,xr.core.dataarray.DataArray):
+           self._cdump = cset
+        else:
+           logger.warning('input not of correct type {}'.format(type(cset)))
+       
+        if 'source' in self._cdump.dims:
+            slist = list(self._cdump.source.values)
+            slist = [str(x) for x in slist]
+            rstr = ','.join(slist)
+            self._source = slist[0]
+            logger.warning('Sources found as a dimension {}. choosing the first one'.format(rstr))
+            self._cdump = self._cdump.isel(source=0) 
+        if 'ens' in self._cdump.dims:
+            slist = list(self._cdump.ens.values)
+            slist = [str(x) for x in slist]
+            rstr = ','.join(slist)
+            self._ens = slist[0]
+            logger.warning('Ensemble found as a dimension {}. choosing the first one'.format(rstr))
+            self._cdump = self._cdump.isel(ens=0) 
+
+    @property
+    def sloc(self):
+        return self._sloc
+
+    @sloc.setter
+    def sloc(self,sloc):
+        if isinstance(sloc,tuple):
+           self._sloc = sloc
+        elif isinstance(sloc, [list,np.ndarray]):
+           self._sloc = tuple(sloc)
+        else:
+           self._sloc=None   
+
+    @property
+    def cmap(self):
+        return self._cmap
+
+    @cmap.setter
+    def cmap(self,cmapstr):
+        self._cmap = plt.get_cmap(cmapstr)
+
+    @property
+    def times(self):
+        return self._cdump.time.values
+
+    @property
+    def levels(self):
+        return self._cdump.z.values
+
+    @property
+    def numpages(self):
+        return len(self._cdump.time.values) 
+
+    @property
+    def rows(self):
+        nnn = len(self.times)
+        return int(np.ceil(nnn/self.columns))
+
+    @property
+    def columns(self):
+        return self._columns
+
+    @columns.setter
+    def columns(self,ccc):
+        self._columns = int(ccc)
+
+
+    @property
+    def figsize(self):
+        return self._figsize
+
+    @figsize.setter
+    def figsize(self, figsize=None):
+        if isinstance(figsize,tuple):
+            self._figsize = figsize
+        else:
+            iii = 4 * self.columns
+            jjj = 4 * self.rows
+            self._figsize = (iii,jjj)
+
+    @property   
+    def unit(self):
+        return self._unit
+
+    @property
+    def central_longitude(self):
+        return self._central_longitude
+    
+    @central_longitude.setter
+    def central_longitude(self,central_longitude):
+        if central_longitude <= 180 and central_longitude >= -180:
+            self._central_longitude = int(central_longitude)
+        else:
+            logger.warning('central longitude out of range -180 to 180')
+            self._central_longitude=0 
+        # always make it positive 180.
+        if central_longitude == -180:
+           self._central_longitude = 180
+
+    @property
+    def transform(self):
+        return get_transform(central_longitude=self.central_longitude)
+
+    @property
+    def data_transform(self):
+        # data always has central_longitude=0
+        return get_transform(central_longitude=0)
+
+
+    def setup_figure(self,fignum=1):
+        # add an extra row for text.
+        fig,axra = plt.subplots(figsize=self.figsize,nrows=self.rows+1,ncols=self.columns,
+                                constrained_layout=False, subplot_kw={'projection':self.transform})
+        return fig, axra
+
+    def makepages(self,pagenums=None,savename='concplot.png'):
+        temp = savename.split('.')
+        base = temp[0]
+        ptype = temp[1]
+        if isinstance(pagenums,list):
+           plist = pagenums
+        else:
+           plist = self.times
+        for iii, pnum in enumerate(plist):
+            savename = '{:s}_{:02d}.{:s}'.format(base,iii,ptype)
+            self.savepage(pnum,savename)
+
+    def savepage(self,pagenum,savename):
+        fig = self.plotpage(pagenum)
+        fig.savefig(savename)
+
+    def plotpage(self):
+      
+         
+        ctemp = hysplit.hysp_massload(self.cdump)
+        ctemp = ctemp # convert from mg/m3 to g/m2.
+
+        # levels and boundaries stay the same on the page.
+        levels = wep.set_levels(ctemp.values)
+        print('levels', levels)
+        #return ctemp
+        xxx = ctemp.longitude.values
+        yyy = ctemp.latitude.values
+        zzz = ctemp.values
+        zzz = np.where(zzz>levels[0]/100.0,zzz,np.nan)
+
+        xtemp = np.where(zzz>levels[0],xxx,np.nan)
+        xmin = np.nanmin(xtemp)
+        xmax = np.nanmax(xtemp)
+
+        # if data is crossing the dateline.
+        if xmin< 0 and xmax>0:
+           self.central_longitude = 180
+           # minimum is now the largest negative number
+           xtemp2 = np.where(xtemp<0,xtemp,np.nan)
+           xmin = np.nanmax(xtemp2)        
+           # maximumis now the smallest positive number
+           xtemp2 = np.where(xtemp>0,xtemp,np.nan)
+           xmax = np.nanmin(xtemp2)        
+           xticks = set_ticks(xmin,xmax,nticks=4)
+           # for setting the limits in the graph,
+           # xmin and xmax have to be set with 180 =0
+           xmax1 = xmax  
+           xmax = 1 * (xmin + self.central_longitude)
+           xmin = -1 * (self.central_longitude - xmax1) 
+        else:
+           xticks = set_ticks(xmin,xmax,nticks=4)
+        xbuffer = 0.1 * (xmax-xmin)
+        if xbuffer > 10: xbuffer=10
+        xmin = xmin-xbuffer
+        xmax = xmax + 2*xbuffer
+
+        ytemp = np.where(zzz>levels[0],yyy,np.nan)
+        ymin = np.nanmin(ytemp)
+        ymax = np.nanmax(ytemp)
+        ybuffer = 0.1 * (ymax-ymin)
+        ymin = ymin - ybuffer
+        ymax = ymax + 2*ybuffer
+ 
+
+        yticks = set_ticks(ymin,ymax,nticks=4)
+
+        norm = BoundaryNorm(levels,ncolors=self.cmap.N,clip=False)
+
+        fig, axlist = self.setup_figure(fignum=1)
+        axtext = []
+        for iii, ax in enumerate(axlist.flatten()):
+            if iii >= len(self.times): 
+               ax.axis('off')
+               axtext.append(ax)
+               continue
+            ctemp2 = ctemp.isel(time=iii)          
+            print(iii, np.nanmax(ctemp2))
+            xxx = ctemp2.longitude.values
+            yyy = ctemp2.latitude.values
+            zzz = ctemp2.values
+            zzz = np.where(zzz>levels[0],zzz,np.nan)
+            cb = ax.pcolormesh(xxx,yyy,zzz,transform=self.data_transform,norm=norm,cmap=self.cmap)   
+            #xmax = 20
+            #xmin = -20
+            print('setting xlimit', xmin, xmax)
+            print('settng ylimit', ymin, ymax)
+            ax.set_xlim(xmin,xmax)
+            ax.set_ylim(ymin,ymax)
+            #ax.set_extent([xmin,xmax,ymin,ymax],crs=ccrs.PlateCarree())
+
+            (row,column) = self.get_row_column(iii)
+            if column==0: ylabel=True
+            else: ylabel=False
+            if row+1==self.rows: xlabel=True
+            else: xlabel=False
+
+            xplace,yplace,size = self.set_text(row)
+            label = pd.to_datetime(ctemp2.time.values)
+            label = label.strftime('%y %m/%d %H UTC')
+            try:
+                ax.text(
+                    xplace,
+                    yplace,
+                    label,
+                    va="bottom",
+                    ha="center",
+                    rotation="horizontal",
+                    rotation_mode="anchor",
+                    transform=ax.transAxes,
+                    size=size,
+                    backgroundcolor="white",
+                )
+            except:
+                pass
+
+            #if isinstance(self.sloc, tuple):
+            #   ax.plot(self.sloc[0],self.sloc[1],'r^',markersize=10)
+
+            format_plot(ax,self.data_transform,fsz=10,xticks=xticks,yticks=yticks,xlabel=xlabel,ylabel=ylabel)
+
+
+
+        cb2 = fig.colorbar(cb,ax=axlist[:,self.columns-1])
+        cb2.ax.tick_params(labelsize=size)
+        cb2.set_label(self.unit,fontsize=size)
+        time = pd.to_datetime(ctemp.time.values)
+        figlabel = attrs2label(self.cdump.attrs, datetime.datetime.now() )
+        axt = axtext[0]
+        axt.text(0,0.0,figlabel,
+                va="bottom",
+                ha="left",
+                rotation="horizontal",
+                rotation_mode="anchor",
+                transform=axt.transAxes,
+                backgroundcolor="white",
+                fontsize=size)
+        #for iii, axi in enumerate(axtext):
+        #    axi.text(0,0,str(iii),va='bottom',ha='left',rotation='horizontal',
+        #             transform = axi.transAxes,backgroundcolor='white',fontsize=size) 
+        return fig
+
+
+
+    def set_text(self,nrow):
+        yplace = 0.95
+        xplace = 0.50
+        size = 10
+        return xplace, yplace, size
+
+
+    def get_row_column(self,iii):
+        column = iii%self.columns
+        row = int(np.floor(iii/self.columns))
+        return (row,column)
+
+    def get_sloc_fromatts(self):
+        attrs = self.cdump.attrs
+        attkeys = list(attrs.keys())
+        check=True
+        if 'latitude' in attkeys:
+           lat = attrs['latitude']
+        else:
+           check=False
+        if 'longitude' in attkeys:
+           lon = attrs['longitude']
+        else:
+           check=False
+        if check: self.sloc = (lon,lat) 
+
+
+def attrs2label(atthash,time,source=None,ens=None):
+    label = 'HYSPLIT\n'
+    label += time.strftime("%Y %m/%d %H:%M UTC ")
+    label += ' 1h average\n'
+    keys = list(atthash.keys())
+    for xxx in ['VolcanoName','Meteorological Model ID']:
+        if xxx in keys:
+            label += '{}: {}\n'.format(xxx, atthash[xxx])
+    for xxx in ['jobid','jobname']:
+        if xxx in keys:
+            label += '{}: {}  '.format(xxx, atthash[xxx])
+    if isinstance(source,str):
+       label += '\n Source: {}'.format(source)
+    if isinstance(ens,str):
+       label += '  MemberID: {}'.format(ens)
+    return label 
+
+
+
--- utilhysplit/plotutils/map_util.py
+++ utilhysplit/plotutils/map_util.py
@@ -5,6 +5,52 @@ import matplotlib.pyplot as plt
 import cartopy
 from cartopy.mpl.gridliner import LATITUDE_FORMATTER, LONGITUDE_FORMATTER
 import matplotlib.ticker as mticker
+import numpy as np
+
+def set_ticks(xmin,xmax,nticks):
+    if xmin<0 and xmax>0:
+       xticks = set_ticks_dateline(xmax,xmin,nticks)
+    elif xmin>0 and xmax<0:
+       xticks = set_ticks_dateline(xmin,xmax,nticks)
+    elif xmax < xmin:
+       xticks = set_ticks_normal(xmax,xmin,nticks)
+    else:
+       xticks = set_ticks_normal(xmin,xmax,nticks)
+    return xticks
+
+def set_ticks_dateline(xmin,xmax,nticks=3):
+    # transform to 0 to 360 coordinates
+    xlimits = [x+360 if x<0 else x for x in [xmin,xmax]]
+    xticks = set_ticks_normal(xlimits[0],xlimits[1])
+    # transform back to -180 to 180 coordinates
+    xticks = [x-360 if x>180 else x for x in xticks]
+    return xticks
+
+
+def set_ticks_normal(xmin,xmax,nticks=3):
+    # if the span is less than one degree
+    if np.abs(xmax-xmin) > 1:
+       first = np.floor(xmin)
+       last = np.ceil(xmax)
+    # round to nearest tenth of degree
+    else:
+       first = np.floor(xmin*10)/10
+       last = np.ceil(xmax*10)/10
+    span = last-first
+    dx = span/nticks
+    print('DX', dx)
+    # round to nearest 5 if greater than 10
+    if np.abs(dx) > 10:
+       print('here')
+       dx = int(int(dx/5.0)*5)
+    elif np.abs(dx) > 1:
+       dx = int(np.floor(dx))
+    else:
+       dx = int(dx*10)/10.0
+    print('DX', dx)
+    rticks = np.arange(first,last+dx,dx)
+    rticks = [int(x*100)/100 for x in rticks]
+    return list(rticks)
 
 
 def draw_map(fignum, ax = None, fs=20):
@@ -30,16 +76,19 @@ def setup_plot():
     mpl.rcParams.update(mpl.rcParamsDefault)
     mpl.rcParams["font.family"] = "sans-serif"
     # mpl.use('pdf')
-    plt.style.use("seaborn-poster")
+    # plt.style.use("seaborn-poster")
     # plt.style.use('fivethirtyeight')
 
 
 def format_plot(ax, transform, 
                 xticks=None,
                 yticks=None, 
+                ylabel=True,
+                xlabel=True,
+                fsz = 20,
                 land=False, borders=False, coastlines=True):
 
-    setup_logger_warning(level=logging.WARNING)
+    #setup_logger_warning(level=logging.WARNING)
     if land: ax.add_feature(cartopy.feature.LAND)
     if borders: ax.add_feature(cartopy.feature.BORDERS)
     if coastlines: ax.coastlines("50m")
@@ -64,16 +113,17 @@ def format_plot(ax, transform,
         linestyle="--",
     )
     gl.top_labels = False
-    gl.bottom_labels = True
+    gl.bottom_labels = xlabel
     gl.right_labels = False
-    gl.left_labels = True
+    gl.left_labels = ylabel
     if isinstance(xticks,list):
        gl.xlocator = mticker.FixedLocator(xticks)
     if isinstance(yticks,list):
        gl.ylocator = mticker.FixedLocator(yticks)
     gl.xformatter = LONGITUDE_FORMATTER
     gl.yformatter = LATITUDE_FORMATTER
-    gl.xlabel_style = {"size": 20, "color": "gray"}
+    gl.xlabel_style = {"size": fsz, "color": "gray"}
+    gl.ylabel_style = {"size": fsz, "color": "gray"}
 
 def get_transform(central_longitude=0):
     transform = cartopy.crs.PlateCarree(central_longitude=central_longitude, globe=None)
